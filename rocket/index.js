c2129eb1970631471c2fe26754009232724cc4e3
Younes Abdennadher <younes.abdennadher@etu.univ-cotedazur.fr>
2020-09-17 19:39:14 +0200

    #2 Switched CI from github action to travis.

--- rocket/index.js
+++ rocket/index.js
@@ -14,4 +14,5 @@ app.get('/rocket', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+    if(process.env.CI !== undefined) process.exit(0)
 })
