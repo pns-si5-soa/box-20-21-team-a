59d99a933ff8d9eded0ac327cd2732e88e3d4ff0
Thomas <thomas.colomban@etu.unice.fr>
2020-09-21 18:00:14 +0200

    #19 renaming mission service

--- command/src/tests/command.test.ts
+++
@@ -1,9 +0,0 @@
-import command from '../command';
-import poll from '../entities/poll'
-
-
-test('Command test', () => {
-	command.createPoll(),
-	expect(command.getPoll()?.rockets).toBe(false),
-	expect(command.getPoll()?.weather).toBe(false)
-});
