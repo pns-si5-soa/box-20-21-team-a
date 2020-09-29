3c21968bde6e46a2f4f9ab2d5177d6f60f649325
AlexandreLon <alexandre.longordo@etu.unice.fr>
2020-09-29 19:05:15 +0200

    #45 Adding material ui

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,5 +1,8 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
+    return <div>Welcome to Next.js! <Button variant="contained">Default</Button></div>
 }
 
 export default HomePage
