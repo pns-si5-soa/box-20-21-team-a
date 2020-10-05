2fa1547bfb79ef606f264ec51ee4c44e06753ad9
AlexandreLon <alexandre.longordo@etu.unice.fr>
2020-10-05 21:13:24 +0200

    #75 Commenting one integrated test (more information on .feature)

--- front/src/tests/integration_testing/features/rocket-service.feature
+++ front/src/tests/integration_testing/features/rocket-service.feature
@@ -1,9 +1,11 @@
 Feature: Rocket
     Get the rocket status
     
-    Scenario: As a Chief of the Rocket department
-        When I consult the current rocket status
-        Then I receive a status code 200 and the rocket status is "Ready,Not ready"
+    # //TODO This scenario must be moved to telemetry-service.feature
+    # For this release we don't have this scenario
+    # Scenario: As a Chief of the Rocket department
+    #     When I consult the current rocket status
+    #     Then I receive a status code 200 and the rocket status is "Ready,Not ready"
 
      Scenario: Launch the rocket twice but only once working
         When The Chief of the Rocket department launches the rocket for the first time
--- front/src/tests/integration_testing/steps/rocket-service.ts
+++ front/src/tests/integration_testing/steps/rocket-service.ts
@@ -12,24 +12,25 @@ let response: any;
 
 const rocketAPI = new RocketAPI();
 
-When("I consult the current rocket status", async function () 
-{
-    response = await rocketAPI.getRocketStatus().then(function(res)
-    {
-        return res;
-    }).catch(function(err)
-    {  
-        return err.response;
-    });
-})
+//TODO See rocket-service.feature for more information
+// When("I consult the current rocket status", async function () 
+// {
+//     response = await rocketAPI.getRocketStatus().then(function(res)
+//     {
+//         return res;
+//     }).catch(function(err)
+//     {  
+//         return err.response;
+//     });
+// })
 
-Then("I receive a status code {int} and the rocket status is {string}", async function(status: number, status_string: String)
-{   
-    const rocket_status: Array<String> = status_string.split(',');
-    expect(await response.status).toEqual(status)
-    expect(await response.data).not.toBe(undefined)
-    expect(rocket_status).toContain(await response.data)
-})
+// Then("I receive a status code {int} and the rocket status is {string}", async function(status: number, status_string: String)
+// {   
+//     const rocket_status: Array<String> = status_string.split(',');
+//     expect(await response.status).toEqual(status)
+//     expect(await response.data).not.toBe(undefined)
+//     expect(rocket_status).toContain(await response.data)
+// })
 
 
 // Second scenario
