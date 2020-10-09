e3f2d04b9a180814f2dc803f39256a932f23cc0a
AlexisLefebvre1953 <alexis.lefebvre@etu.univ-cotedazur.fr>
2020-10-09 20:40:11 +0200

    #111 update payloadData to match data sent from payload

--- back/telemetry-service/src/app/entities/Payload/PayloadData.ts
+++ back/telemetry-service/src/app/entities/Payload/PayloadData.ts
@@ -3,18 +3,40 @@ import PayloadStatus from "./PayloadStatus";
 
 class PayloadData{
     private payloadStatus: PayloadStatus;
+    private speed : number;
+    private altitude : number;
 
-    constructor(payloadStatus=PayloadStatus.ATTACHED) {
+    constructor(payloadStatus=PayloadStatus.ATTACHED,altitude = -1,speed = -1) {
         this.payloadStatus=payloadStatus;
+        this.speed = speed;
+        this.altitude = altitude;
     }
 
-    getPayloadStatus(){
+    getPayloadStatus() : PayloadStatus{
         return this.payloadStatus;
     }
 
     setPayloadStatus(value : number){
         this.payloadStatus=value;
     }
+
+    getAltitude(): number{
+        return this.altitude;
+    }
+
+    setAltitude(value : number){
+        this.altitude=value;
+    }
+
+    getSpeed() : number{
+        return this.speed;
+    }
+
+    setSpeed(value : number){
+        this.speed=value;
+    }
+
+
 }
 
 export default PayloadData
