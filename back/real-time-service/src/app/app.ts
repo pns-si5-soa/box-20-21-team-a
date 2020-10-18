50ad1e873a1058caf0a117151df80c880dd2ca09
AlexisLefebvre1953 <alexis.lefebvre@etu.univ-cotedazur.fr>
2020-10-18 08:52:18 +0200

    #156 receiver kafka on real time, first shot on app.ts maybe to move later

--- back/real-time-service/src/app/app.ts
+++ back/real-time-service/src/app/app.ts
@@ -95,3 +95,56 @@ function onListening() {
         process.exit(0);
     }
 }
+
+const { Kafka, logLevel } = require('kafkajs')
+const host = process.env.HOST_IP;
+
+
+
+const kafka = new Kafka({
+  logLevel: logLevel.INFO,
+  brokers: [`${host}:9092`],
+  clientId: 'example-consumer',
+})
+
+const topic = 'mission-topic'
+const consumer = kafka.consumer({ groupId: 'test-group' })
+
+const run = async () => {
+  await consumer.connect()
+  await consumer.subscribe({ topic, fromBeginning: true })
+  await consumer.run({
+     eachBatch: async ({ batch } : any) => {
+        console.log(" - - -- - - - -- - - batch  - - - -- - - - - ")
+       console.log(batch)
+     },
+    eachMessage: async ({ topic, partition, message } : any) => {
+      console.log(" - -- -- - - - - - - - -each message - - - - -- - - - - - - - - ");
+
+      const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
+
+      console.log(`- ${prefix} ${message.key}#${message.value}`)
+      console.log(" - -- -- - - - - - - - -.... - - - - -- - - - - - - - - ");
+
+    },
+  })
+
+}
+
+run().catch(e => console.error(`[example/consumer] ${e.message}`, e))
+
+const errorTypes = ['unhandledRejection', 'uncaughtException']
+const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
+
+errorTypes.map(type => {
+  process.on(type, async e => {
+    try {
+      console.log(`process.on ${type}`)
+      console.error(e)
+      await consumer.disconnect()
+      process.exit(0)
+    } catch (_) {
+      process.exit(1)
+    }
+  })
+})
