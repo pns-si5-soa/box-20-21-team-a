9ccba5d2c7fc3b4f181a8650b9a8c5049e35f6b8
AlexandreLon <alexandre.longordo@etu.unice.fr>
2020-10-21 16:28:20 +0200

    #168 Create telemetry-listener from telemetry

---
+++ back/telemetry-listener-service/src/app/entities/Entitie.ts
@@ -0,0 +1,70 @@
+import {insertOne, removeOne, update, find} from "../db/Mongo"
+import BoosterData from "./Booster/BoosterData";
+
+abstract class Entitie {
+
+    protected id: number;
+
+    constructor() {
+        this.id = -1;
+    }
+
+    save() {
+        insertOne(this.constructor.name, this.toObjectJSON()).then(res => {
+            this.id = res.insertedIds
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    delete() {
+        if(this.id == -1) throw new Error("Impossible to remove because is not in db")
+        removeOne(this.constructor.name, {id: this.id}).catch(err => {
+            console.error(err)
+        })
+    }
+
+    update() {
+        if(this.id == -1) throw new Error("Impossible to remove because is not in db")
+        update(this.constructor.name, this.toObjectJSON(), {id: this.id}).catch(err => {
+            console.error(err)
+        })
+    }
+
+    findIdAndAssign<T extends Entitie>(id: number) : Promise<Entitie> {
+        return new Promise((resolve, reject) => {
+            find<T>(this.constructor.name, {id: id}).then(res => {
+                if(res === undefined) reject(this.constructor.name + " has not object with id " + id)
+                this.assign(res[0])
+                resolve(this)
+            }).catch(err => reject(err))
+        }) 
+    }
+
+    findFirstAndAssign<T extends Entitie>() : Promise<Entitie> {
+        return new Promise((resolve, reject) => {
+            find<T>(this.constructor.name, {}, {_id: 1}).then(res => {
+                if(res === undefined || res.length === 0 ) reject(this.constructor.name + " has not object")
+                this.assign(res[0])
+                resolve(this)
+            }).catch(err => reject(err))
+        })
+    }
+
+    findLastAndAssign<T extends Entitie>() : Promise<Entitie> {
+        return new Promise((resolve, reject) => {
+            find<T>(this.constructor.name, {}, {_id: -1}).then(res => {
+                if(res === undefined || res.length === 0 ) reject(this.constructor.name + " has not object")
+                this.assign(res[0])
+                resolve(this)
+            }).catch(err => reject(err))
+        })
+    }
+
+    //Serialize to database
+    abstract toObjectJSON() : Object;
+    //Unserialise from database
+    abstract assign(other: any) : void;
+}
+
+export default Entitie;
