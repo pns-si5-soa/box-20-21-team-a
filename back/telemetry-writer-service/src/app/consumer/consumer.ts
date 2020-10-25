a9a4d1352a1b250a1a7d6c52013fbbe86f5701e6
AlexandreLon <alexandre.longordo@etu.unice.fr>
2020-10-25 15:35:00 +0100

    #169 Listen kafka on writer

---
+++ back/telemetry-writer-service/src/app/consumer/consumer.ts
@@ -0,0 +1,51 @@
+// KAFKA
+const { Kafka, logLevel } = require('kafkajs');
+const host = process.env.HOST_IP ?? '127.0.0.1';
+
+const kafka = new Kafka({
+	logLevel: logLevel.INFO,
+	brokers: [`${host}:9092`],
+	clientId: 'example-consumer',
+});
+
+const consumer = kafka.consumer({ groupId: 'test-group' });
+
+const run = async (topic_name : string, callback: (response: string) => void) => {
+	await consumer.connect();
+	await consumer.subscribe({ topic: topic_name, fromBeginning: true });
+	await consumer.run({
+		eachBatch: async ({ batch }: any) => {
+			console.log(batch);
+		},
+		eachMessage: async ({ topic, partition, message }: any) => {
+			const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`;
+
+			console.log(`- ${prefix} ${message.key}#${message.value}`);
+			var msg = message.value;
+			var json = JSON.parse(msg);
+			if (topic == topic_name) {
+				callback(json)
+			}
+		},
+	});
+};
+
+// run().catch((e) => console.error(`[example/consumer] ${e.message}`, e));
+
+const errorTypes = ['unhandledRejection', 'uncaughtException'];
+const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2'];
+
+errorTypes.map((type) => {
+	process.on(type, async (e) => {
+		try {
+			console.log(`process.on ${type}`);
+			console.error(e);
+			await consumer.disconnect();
+			process.exit(0);
+		} catch (_) {
+			process.exit(1);
+		}
+	});
+});
+
+export {run};
