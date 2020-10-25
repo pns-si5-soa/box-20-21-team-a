c3122b0746f78fd72e6805ab3df444ddb276ac87
AlexisLefebvre1953 <alexis.lefebvre@etu.univ-cotedazur.fr>
2020-10-25 15:10:08 +0100

    #166 update tests on payload/mission/rocket

--- back/payload-service/src/tests/payload.test.ts
+++ back/payload-service/src/tests/payload.test.ts
@@ -1,5 +1,5 @@
 import Payload from "../app/aggregate/Payload";
-import PayloadController from "../app/controller/PayloadController";
+import payloadController from "../app/controller/PayloadController";
 import PayloadStatus from "../app/entities/PayloadStatus";
 
 test('not launched payload init values', () => {
@@ -11,9 +11,9 @@ test('not launched payload init values', () => {
 });
 
 test('detach process', async ()  => {
-    const payloadController = new PayloadController();
-    await payloadController.detachThePayload();
-    const datas = payloadController.payload.payloadData.toObjectJSON();
+    payloadController.addNewPayload(0)
+    await payloadController.detachThePayload(0);
+    const datas = payloadController.payloads[0].payloadData.toObjectJSON();
     expect(datas.altitude).toBeLessThanOrEqual(36000);
     expect(datas.payloadStatus).toBe(PayloadStatus.DETACHED);
 });
