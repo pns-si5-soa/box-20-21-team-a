3d32e2146c311e3e67b4ed65dfa7bedd1050b6a8
Thomas <thomas.colomban@etu.unice.fr>
2020-10-25 20:28:33 +0100

    #176 adding anomaly enum

---
+++ front/src/main/model/Anomaly.ts
@@ -0,0 +1,12 @@
+export enum Anomaly {
+    BOOSTER_FUEL_EMPTY,
+    ROCKET_FUEL_EMPTY,
+    ROCKET_FALLING,
+}
+
+
+export const mapAnomalyToText = {
+    0: "Booster Fuel Empty",
+    1: "Rocket Fuel Empty",
+    2: "Rocket Falling",
+};
