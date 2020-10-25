096d9328fe645f0a3bd1e246937130572d3ff419
AlexisLefebvre1953 <alexis.lefebvre@etu.univ-cotedazur.fr>
2020-10-25 14:46:29 +0100

    #166 update payload : 1 to N payload

--- back/payload-service/src/app/controller/PayloadController.ts
+++ back/payload-service/src/app/controller/PayloadController.ts
@@ -4,32 +4,41 @@ import MissionAPI from "../API/missionAPI";
 import MissionPayloadStatus from "../entities/MissionPayloadStatus";
 
 
-export default class PayloadController {
+class PayloadController {
 
     private telemetryAPI: TelemetryAPI = new TelemetryAPI();
     private missionAPI: MissionAPI = new MissionAPI();
 
-    payload: Payload;
+    payloads : Payload[];
 
 
     constructor() {
-        this.payload = new Payload();
+        this.payloads = [];
     }
 
-    async detachThePayload() {
-        this.payload.detach();
+    addNewPayload(payloadId :number) : number{
+        let payload = new Payload(); 
+           this.payloads[payloadId] = payload;
+        return payloadId; 
+    }
+
+    async detachThePayload(id : number) {
+        this.payloads[id].detach();
         if (process.env.NODE_ENV != "test") {
-            this.telemetryAPI.sendPayloadData(this.payload.payloadData);
+            this.telemetryAPI.sendPayloadData(this.payloads[id].payloadData);
         }
         if (process.env.NODE_ENV != "test") {
+            // TODO 
             this.missionAPI.sendPayloadSeparationStatus(MissionPayloadStatus.PAYLOAD_SEPARATION_STATUS_FOR_MISSION);
         }
-        await this.payload.progressingToOrbitalPosition();
+        await this.payloads[id].progressingToOrbitalPosition();
         var that = this;
         if (process.env.NODE_ENV != "test") {
-            that.telemetryAPI.sendPayloadData(that.payload.payloadData);
+            that.telemetryAPI.sendPayloadData(that.payloads[id].payloadData);
         }
     }
 
 
 }
+
+export default new PayloadController;
--- back/payload-service/src/app/routes/index.ts
+++ back/payload-service/src/app/routes/index.ts
@@ -1,6 +1,10 @@
 import express = require('express')
 import Payload from '../aggregate/Payload';
 const router = express.Router(); 
+import payloads from './payloads';
+
+
+router.use("/payloads", payloads)
 
 export default router;
 
