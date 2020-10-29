4549bd9bd936bc0e7d12a2a9d3f9d0a6c0c4f985
AlexisLefebvre1953 <alexis.lefebvre@etu.univ-cotedazur.fr>
2020-10-29 22:51:31 +0100

    #187 id is now uuid string and so table is now an object

--- back/real-time-service/src/app/routes/mission/index.ts
+++ back/real-time-service/src/app/routes/mission/index.ts
@@ -4,11 +4,11 @@ const router = express.Router();
 import mission from "../../controller/mission"
 
 router.get('/:id', (req, res) => {
-    res.status(200).json(mission.getStatus(parseInt(req.params.id)));
+    res.status(200).json(mission.getStatus(req.params.id));
 });
 
 router.post('/', (req, res) => {
-    res.send({'created': mission.addNewMission(parseInt(req.body.id))});
+    res.send({'created': mission.addNewMission(req.body.id)});
     
 });
 
