8125aaeeaae3636c574bcb74c5ce3b280200d7bd
AlexandreLon <alexandre.longordo@etu.unice.fr>
2020-11-01 14:49:11 +0100

    #194 Create generator of stepdef and generate complete scenario stepdef

---
+++ front/src/tests/integration_testing/steps/complete-mission.ts
@@ -0,0 +1,132 @@
+const {
+	When,
+	Then,
+	Given,
+} = require('cucumber');
+
+When("Richard creates a new mission", function() {
+	//do something
+})
+Then("a new poll is created with its values set as {boolean}", function(arg0: boolean) {
+	//do something
+})
+Then("a new head stage is created #tester l'existance en faisant un get sur la télémétrie", function() {
+	//do something
+})
+Then("a new booster is created", function() {
+	//do something
+})
+Then("a new payload is creaded", function() {
+	//do something
+})
+When("Tory has been informed that the poll has been created, she can check the weather status", function() {
+	//do something
+})
+Then("the weather status is {string}, {string} or {string}", function(arg0: string, arg1: string, arg2: string) {
+	//do something
+})
+When("she answers positively to the poll", function() {
+	//do something
+})
+Then("the weather department vote is {boolean}", function(arg0: boolean) {
+	//do something
+})
+When("Elon gets the poll", function() {
+	//do something
+})
+Then("he sees that Tory has answered to it so it is his turn to answer", function() {
+	//do something
+})
+When("he gets the rocket status", function() {
+	//do something
+})
+Then("he sees the rocket is {string}", function(arg0: string) {
+	//do something
+})
+When("Elon puts the rocket on internal power", function() {
+	//do something
+})
+Then("the status of the rocket is now {string} and everything is going fine for the rocket", function(arg0: string) {
+	//do something
+})
+When("he answers positively to the poll", function() {
+	//do something
+})
+Then("the rocket department answer to the poll is now {boolean}", function(arg0: boolean) {
+	//do something
+})
+When("it{string}s turn to answer, he votes on the poll", function(arg0: string) {
+	//do something
+})
+Then("the poll shows that the mission department vote is {boolean}", function(arg0: boolean) {
+	//do something
+})
+When("Elon initializes the launch process", function() {
+	//do something
+})
+Then("Richard and Marie see in real time that the status of the rocket is now on {string}", function(arg0: string) {
+	//do something
+})
+When("{number} seconds have passed", function(arg0: number) {
+	//do something
+})
+Then("Richard and Marie see in real time that the rocket status is now {string}", function(arg0: string) {
+	//do something
+})
+When("{number} seconds have passed", function(arg0: number) {
+	//do something
+})
+Then("Richard and Marie see in real time that the rocket is {string}", function(arg0: string) {
+	//do something
+})
+When("the pressure of the rocket is at {number}?", function(arg0: number) {
+	//do something
+})
+Then("Richard and Marie see in real time that the rocket status is now {string}", function(arg0: string) {
+	//do something
+})
+Then("the speed of the rocket does not increase anymore", function() {
+	//do something
+})
+When("the booster has {number}hl of fuel left", function(arg0: number) {
+	//do something
+})
+Then("Richard and Marie see in real time that the rocket status is {string}", function(arg0: string) {
+	//do something
+})
+Then("then the rocket status is {string}", function(arg0: string) {
+	//do something
+})
+Then("then the rocket status is {string}", function(arg0: string) {
+	//do something
+})
+Then("that the booster is going through a {string}", function(arg0: string) {
+	//do something
+})
+Then("then the booster status is {string}", function(arg0: string) {
+	//do something
+})
+Then("then the booster status is {string}", function(arg0: string) {
+	//do something
+})
+Then("then the booster status is Landing burn'", function() {
+	//do something
+})
+Then("then the booster status is Landing legs deployed", function() {
+	//do something
+})
+Then("then the booster has landed, its status is {string}", function(arg0: string) {
+	//do something
+})
+Then("then the rocket status is {string}", function(arg0: string) {
+	//do something
+})
+When("the rocket has no more fuel", function() {
+	//do something
+})
+Then("the rocket status is Second engine cut off", function() {
+	//do something
+})
+When("Gwynn decides to deliver the payload then the rocket status is now {string}", function(arg0: string) {
+	//do something
+})
