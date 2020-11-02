2a49ba4f977480faf17bf6f73e430348f80de817
AlexandreLon <alexandre.longordo@etu.unice.fr>
2020-11-02 13:57:10 +0100

    #194 Ignore old scenario cucumber

--- front/src/tests/ignored/steps/mission-service.ts
+++ front/src/tests/ignored/steps/mission-service.ts
@@ -9,15 +9,17 @@ require('dotenv').config()
 import expect from 'expect';
 import MissionAPI from '../../../main/API/rest/missionAPI';
 import MissionsCoordinatorAPI from '../../../main/API/rest/missionsCoordinatorAPI';
+import PollAPI from '../../../main/API/rest/pollAPI';
 
 let response: any;
 
 const missionAPI = new MissionAPI();
+const pollAPI = new PollAPI();
 const missionsCoordinatorAPI = new MissionsCoordinatorAPI();
 
 When('I create a Go\\/No Go poll', async function ()
 {
-    response = await missionAPI.createPoll().then(function(res)
+    response = await missionsCoordinatorAPI.createMission().then(function(res)
     {
         return res;
     }).catch(function(err)
@@ -37,14 +39,14 @@ Then("I receive a status code {int} and the parameters are {string} by default",
 
 When('I received a {string} answer from rocket service,I modify the poll', async function (answer : string)
 {
-    const missionId = await missionAPI.createPoll().then((res) => {
+    const missionId = await missionsCoordinatorAPI.createMission().then((res) => {
         return missionsCoordinatorAPI.getMissions().then((res) =>{
             return res.data;
         });
     }).catch(err => {
         console.error('There is an error : ', err);
     })
-    response = await missionAPI.modifyPoll("rocket", answer,missionId).then(function(res)
+    response = await pollAPI.modifyPoll("rocket", answer,missionId).then(function(res)
     {
         return res;
     }).catch(function(err)
@@ -56,21 +58,21 @@ When('I received a {string} answer from rocket service,I modify the poll', async
 
 Then("The new rocket status is {string}", async function(status: string)
     {
-        expect((await missionAPI.getPoll()).data.rocketStatus.toString()).toEqual(status);
+        expect((await pollAPI.getPoll()).data.rocketStatus.toString()).toEqual(status);
     }
 )
 
 
 When('I received a {string} answer from weather service,I modify the poll', async function (answer : string)
 {
-    const missionId = await missionAPI.createPoll().then((res) => {
+    const missionId = await missionsCoordinatorAPI.createMission().then((res) => {
         return missionsCoordinatorAPI.getMissions().then((res) =>{
             return res.data;
         });
     }).catch(err => {
         console.error('There is an error : ', err);
     })
-    response = await missionAPI.modifyPoll("weather", answer,missionId).then(function(res)
+    response = await pollAPI.modifyPoll("weather", answer,missionId).then(function(res)
     {
         return res;
     }).catch(function(err)
@@ -82,6 +84,6 @@ When('I received a {string} answer from weather service,I modify the poll', asyn
 
 Then("The new weather status is {string}", async function(status: boolean)
     {
-        expect((await missionAPI.getPoll()).data.weatherStatus.toString()).toEqual(status);
+        expect((await pollAPI.getPoll()).data.weatherStatus.toString()).toEqual(status);
     }
 )
