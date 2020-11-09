a654557ef2263b98b4e58a979ddfaa42ebcb69d6
YounesAbdennadher <younes.abdennadher@etu.univ-cotedazur.fr>
2020-11-09 15:18:35 +0100

    #200 Fixed anomaly handler service that sent abort mission to Mission.

--- back/anomaly-handler-service/src/app/controller/AnomalyHandlerService.ts
+++ back/anomaly-handler-service/src/app/controller/AnomalyHandlerService.ts
@@ -18,7 +18,7 @@ class AnomalyAnalyserService {
     analyseAnomaly(id : string, anomaly : AnomalyEnum){
         let newAnomaly = new RocketAnomaly(id, anomaly);
         if (newAnomaly.anomalyIsCritical()){
-            missionAPI.sendData(RocketStatus.ABORTED_AND_ROCKET_DESTROYED, id);
+            // missionAPI.sendData(RocketStatus.ABORTED_AND_ROCKET_DESTROYED, id);
             boosterAPI.destroyBoosterSOAPBack(id);
             rocketAPI.destroyRocketSOAPBack(id);
         }
