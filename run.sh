ba6bf8edaddf7db530f9dba2e753e7b4d1d12888
AlexandreLon <alexandre.longordo@etu.unice.fr>
2020-11-11 17:01:22 +0100

    Remove useless log

--- run.sh
+++ run.sh
@@ -2,6 +2,5 @@ yellow=`tput setaf 3`
 docker-compose -f docker-compose.yml up \
     --abort-on-container-exit \
     --exit-code-from front
-cat container-logs/*.log
 echo $yellow 
 echo "You can see logs in the container-logs directory"
